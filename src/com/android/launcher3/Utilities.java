a64388087aa68420d808649e599612fddecb2246
ElDainosor <eldainosor@gmail.com>
2018-11-01 21:22:10 -0500

    Set a toggle to show Search bar on first screen
    Instead of hardcoding it on the FeatureFlags, let's make it
    toggleable as any other option would do
    
    Change-Id: I59893308efa357d25bdd3d53e31904e90c4d84ca
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -88,6 +88,8 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.android.internal.util.nitrogen.NitrogenUtils;
+
 /**
  * Various utilities shared amongst the Launcher's classes.
  */
@@ -124,6 +126,8 @@ public final class Utilities {
      */
     public static final int EDGE_NAV_BAR = 1 << 8;
 
+    static final String KEY_SHOW_SEARCHBAR = "pref_show_searchbar";
+
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
      * add extra logging and not for changing the app behavior.
@@ -140,6 +144,8 @@ public final class Utilities {
     // An intent extra to indicate the horizontal scroll of the wallpaper.
     public static final String EXTRA_WALLPAPER_OFFSET = "com.android.launcher3.WALLPAPER_OFFSET";
     public static final String EXTRA_WALLPAPER_FLAVOR = "com.android.launcher3.WALLPAPER_FLAVOR";
+
+    public static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
 
     public static boolean IS_RUNNING_IN_TEST_HARNESS =
                     ActivityManager.isRunningInTestHarness();
@@ -691,4 +697,13 @@ public final class Utilities {
             return mSize;
         }
     }
+
+    public static boolean showQSB(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        if (!SyberiaUtils.isPackageInstalled(context, SEARCH_PACKAGE)) {
+            return false;
+        }
+        return prefs.getBoolean(KEY_SHOW_SEARCHBAR, true);
+    }
+
 }
