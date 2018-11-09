fe0e1810905e3586efe8f227ab565e7492e13373
ezio84 <brabus84@gmail.com>
2018-11-09 22:15:53 +0100

    Launcher3: Skip unneded package manager calls when checking QSB availability
    Previous qsb toggle commit added a Search package availability check
    that was being called also on some config changes (e.g. open an app,
    rotate the screen, press home).
    We can use the existing callbacks to avoid this.

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -69,8 +69,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.android.internal.util.nitrogen.NitrogenUtils;
-
 /**
  * Various utilities shared amongst the Launcher's classes.
  */
@@ -705,7 +703,7 @@ public final class Utilities {
 
     public static boolean showQSB(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
-        if (!NitrogenUtils.isPackageInstalled(context, LauncherTab.SEARCH_PACKAGE)) {
+        if (!LauncherAppState.getInstanceNoCreate().isSearchAppAvailable()) {
             return false;
         }
         return prefs.getBoolean(Homescreen.KEY_SHOW_SEARCHBAR, true);
