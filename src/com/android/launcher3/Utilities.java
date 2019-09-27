84ffcb228a846f95017f173df4a1615b61836604
DennySPB <dennyspb@gmail.com>
2019-09-27 15:21:57 +0300

    Launcher3: Add toggle for DT2S
    Change-Id: I22abb02de7394094653d571e20eee0e93fb1e9a5
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -145,6 +145,8 @@ public final class Utilities {
      */
     public static final int EDGE_NAV_BAR = 1 << 8;
 
+    public static final String KEY_DT_GESTURE = "pref_dt_gesture";
+
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
      * add extra logging and not for changing the app behavior.
@@ -933,5 +935,10 @@ public final class Utilities {
 
     public static boolean bothNonNull(@Nullable Object a, @Nullable Object b) {
         return a != null && b != null;
+    }
+
+    public static boolean isDoubleTapGestureEnabled(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DT_GESTURE, true);
     }
 }
